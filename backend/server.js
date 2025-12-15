8ec4ff44d3613bb70f5b95288bd9aec812ecea53
phuongwj <phamjulia9@gmail.com>
2025-12-15 16:47:16 -0400

    updated server.js cause i'll be putting my frontend on s3

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,11 @@ dotenv.config();
 
 import express from 'express';
 import bodyParser from 'body-parser';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 import router from './routes/messageRoutes.js';
 
 const app = express();
 const port = 8000;
-
-/* ES modules __dirname */
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 /* Parses incoming request bodies with a Content-Type of application/json */
 app.use(bodyParser.json());
@@ -27,15 +21,6 @@ app.use(
     extended: true,
   })
 )
-
-// Serves frontend as static files from ../frontend
-const frontendPath = path.join(__dirname, '..', 'frontend');
-app.use(express.static(frontendPath));
-
-// Root route -> message.html
-app.get('/', (request, response) => {
-  response.sendFile(path.join(frontendPath, 'message.html'));
-});
 
 // API Routes
 app.use(router);
